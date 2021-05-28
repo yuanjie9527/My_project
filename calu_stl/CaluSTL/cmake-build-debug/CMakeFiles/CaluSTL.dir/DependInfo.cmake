5aa26be755350e43014b5c3801048e510bfcbed3
yuanjie9527 <1608803065@qq.com>
2021-05-28 21:36:08 +0800

    delete cmake

---
+++ calu_stl/CaluSTL/cmake-build-debug/CMakeFiles/CaluSTL.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zyj/My_project/calu_stl/CaluSTL/Point3f.cpp" "/home/zyj/My_project/calu_stl/CaluSTL/cmake-build-debug/CMakeFiles/CaluSTL.dir/Point3f.cpp.o"
+  "/home/zyj/My_project/calu_stl/CaluSTL/ReadSTLFile.cpp" "/home/zyj/My_project/calu_stl/CaluSTL/cmake-build-debug/CMakeFiles/CaluSTL.dir/ReadSTLFile.cpp.o"
+  "/home/zyj/My_project/calu_stl/CaluSTL/TrianglesArea.cpp" "/home/zyj/My_project/calu_stl/CaluSTL/cmake-build-debug/CMakeFiles/CaluSTL.dir/TrianglesArea.cpp.o"
+  "/home/zyj/My_project/calu_stl/CaluSTL/main.cpp" "/home/zyj/My_project/calu_stl/CaluSTL/cmake-build-debug/CMakeFiles/CaluSTL.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ calu_stl/ReadSTL/cmake-build-debug/CMakeFiles/ReadSTL.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zyj/My_project/calu_stl/ReadSTL/Point3f.cpp" "/home/zyj/My_project/calu_stl/ReadSTL/cmake-build-debug/CMakeFiles/ReadSTL.dir/Point3f.cpp.o"
+  "/home/zyj/My_project/calu_stl/ReadSTL/ReadSTLFile.cpp" "/home/zyj/My_project/calu_stl/ReadSTL/cmake-build-debug/CMakeFiles/ReadSTL.dir/ReadSTLFile.cpp.o"
+  "/home/zyj/My_project/calu_stl/ReadSTL/TrianglesArea.cpp" "/home/zyj/My_project/calu_stl/ReadSTL/cmake-build-debug/CMakeFiles/ReadSTL.dir/TrianglesArea.cpp.o"
+  "/home/zyj/My_project/calu_stl/ReadSTL/main.cpp" "/home/zyj/My_project/calu_stl/ReadSTL/cmake-build-debug/CMakeFiles/ReadSTL.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
